c7d0d3d4cb7d82f9cc5ba81323c87277b0fe0405
Erick Giraldo <erickgiraldo94@gmail.com>
2024-10-25 05:18:15 -0500

    feat: Added confirmation modal for deletion

---
+++ src/components/common/confirmation-modal.component.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import {Modal, StyleSheet, TouchableOpacity, View} from 'react-native';
+import {Text} from 'react-native-elements';
+
+interface IConfirmationModal {
+  children: React.ReactNode;
+  confirmationModalVisible: boolean;
+  setConfirmatioModalVisible: (confirmationModalVisible: boolean) => void;
+  handleAccept: () => void;
+}
+
+export const ConfirmationModal = ({
+  children,
+  confirmationModalVisible,
+  setConfirmatioModalVisible,
+  handleAccept,
+}: IConfirmationModal): React.JSX.Element => {
+  return (
+    <View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={confirmationModalVisible}
+        onRequestClose={() => {
+          setConfirmatioModalVisible(!confirmationModalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>{children}</Text>
+            <TouchableOpacity
+              style={[styles.button, styles.buttonClose]}
+              onPress={handleAccept}>
+              <Text>Accept</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => {
+                setConfirmatioModalVisible(!confirmationModalVisible);
+              }}>
+              <Text>Cancel</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
--- src/screens/contact-details.screen.tsx
+++ src/screens/contact-details.screen.tsx
@@ -4,8 +4,9 @@ import {RootStackParamList} from '../interfaces';
 import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
 import ContactImage from '../components/common/contactImage.component';
 import {useContactById} from '../hooks/useContactById.hook';
-import React from 'react';
+import React, {useState} from 'react';
 import {ContactsService} from '../services/contacts.service';
+import {ConfirmationModal} from '../components/common/confirmation-modal.component';
 
 type ContactDetailsScreenProp = NativeStackNavigationProp<
   RootStackParamList,
@@ -20,6 +21,9 @@ export function ContactDetailsScreen(): React.JSX.Element {
 
   const {contactInfo, isContactLoading, errorLoadingContact} =
     useContactById(contactId);
+
+  const [confirmationModalVisible, setConfirmatioModalVisible] =
+    useState<boolean>(false);
 
   const handleDeleteContact = async () => {
     await ContactsService.delete(contactId);
@@ -46,9 +50,15 @@ export function ContactDetailsScreen(): React.JSX.Element {
             }>
             <Text>Edit contact</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={handleDeleteContact}>
+          <TouchableOpacity onPress={() => setConfirmatioModalVisible(true)}>
             <Text>Delete contact</Text>
           </TouchableOpacity>
+          <ConfirmationModal
+            confirmationModalVisible={confirmationModalVisible}
+            setConfirmatioModalVisible={setConfirmatioModalVisible}
+            handleAccept={handleDeleteContact}>
+            <Text>Do you want to delete this contact?</Text>
+          </ConfirmationModal>
         </View>
       )}
     </View>
