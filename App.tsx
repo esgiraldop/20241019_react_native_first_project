6f70d195dc6aed40437ea378a9d256e7068bd5f3
Erick Giraldo <erickgiraldo94@gmail.com>
2024-11-14 16:48:11 -0500

    feat: added splash screen

--- App.tsx
+++ App.tsx
@@ -2,8 +2,8 @@
 //   require('./ReactotronConfig');
 // }
 
-import React from 'react';
-import {StatusBar, useColorScheme} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StatusBar, useColorScheme, View} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 // import {DemoScreen} from './src/screens/demo.screen';
 import {NavigationContainer} from '@react-navigation/native';
@@ -20,71 +20,90 @@ import {theme} from './src/theme/main.theme';
 import {RegistrationScreen} from './src/screens/register.screen';
 import {LoginScreen} from './src/screens/login.screen';
 import {SyncProvider} from './src/contexts/contacts-syncronization.context';
+import AppSplashScreen from './src/components/register/App-splash-screen.component';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App(): React.JSX.Element {
+  const [showSplash, setShowSplash] = useState(true);
   const isDarkMode = useColorScheme() === 'dark'; // TODO: Maybe define this in the main app theme?
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  useEffect(() => {
+    // Set a timer to transition to the main app
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 2000); // splash duration
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <SafeAreaProvider style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <SyncProvider>
-        <NavigationContainer>
-          <Stack.Navigator
-            initialRouteName="Register"
-            screenOptions={{
-              headerStyle: {
-                backgroundColor: theme.colors.background,
-              },
-              headerTintColor: theme.colors.textPrimary,
-              headerTitleStyle: {
-                fontSize: theme.fontSizes.title,
-                color: theme.colors.textPrimary,
-              },
-              animation: 'slide_from_right',
-              freezeOnBlur: true,
-            }}>
-            {/* <Stack.Screen name="Demo" component={DemoScreen} /> */}
-            <Stack.Screen
-              name="Register"
-              component={RegistrationScreen}
-              options={{title: 'User registration'}}
+      <View style={{flex: 1}}>
+        {showSplash ? (
+          <AppSplashScreen />
+        ) : (
+          <>
+            <StatusBar
+              barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+              backgroundColor={backgroundStyle.backgroundColor}
             />
-            <Stack.Screen
-              name="Login"
-              component={LoginScreen}
-              options={{title: 'User login'}}
-            />
-            <Stack.Screen
-              name="Contacts"
-              component={AllContactsScreen}
-              options={{title: 'Contacts'}}
-            />
-            <Stack.Screen
-              name="ContactDetails"
-              component={ContactDetailsScreen}
-              options={{title: 'Contact details'}}
-            />
-            <Stack.Screen
-              name="AddContact"
-              component={AddContactScreen}
-              options={{title: 'Add new contact'}}
-            />
-            <Stack.Screen
-              name="EditContact"
-              component={EditContactScreen}
-              options={{title: 'Edit contact'}}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </SyncProvider>
+            <SyncProvider>
+              <NavigationContainer>
+                <Stack.Navigator
+                  initialRouteName="Register"
+                  screenOptions={{
+                    headerStyle: {
+                      backgroundColor: theme.colors.background,
+                    },
+                    headerTintColor: theme.colors.textPrimary,
+                    headerTitleStyle: {
+                      fontSize: theme.fontSizes.title,
+                      color: theme.colors.textPrimary,
+                    },
+                    animation: 'slide_from_right',
+                    freezeOnBlur: true,
+                  }}>
+                  {/* <Stack.Screen name="Demo" component={DemoScreen} /> */}
+                  <Stack.Screen
+                    name="Register"
+                    component={RegistrationScreen}
+                    options={{title: 'User registration'}}
+                  />
+                  <Stack.Screen
+                    name="Login"
+                    component={LoginScreen}
+                    options={{title: 'User login'}}
+                  />
+                  <Stack.Screen
+                    name="Contacts"
+                    component={AllContactsScreen}
+                    options={{title: 'Contacts'}}
+                  />
+                  <Stack.Screen
+                    name="ContactDetails"
+                    component={ContactDetailsScreen}
+                    options={{title: 'Contact details'}}
+                  />
+                  <Stack.Screen
+                    name="AddContact"
+                    component={AddContactScreen}
+                    options={{title: 'Add new contact'}}
+                  />
+                  <Stack.Screen
+                    name="EditContact"
+                    component={EditContactScreen}
+                    options={{title: 'Edit contact'}}
+                  />
+                </Stack.Navigator>
+              </NavigationContainer>
+            </SyncProvider>
+          </>
+        )}
+      </View>
     </SafeAreaProvider>
   );
 }
